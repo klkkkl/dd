74b4488f4ebecc18a172edb6996de2b5653ae3ef
高帅 <yanghui.lyh@alibaba-inc.com>
2014-10-17 11:46:43 +0800

    create tower

---
+++ Tower/Tower/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Tower.Startup))]
+namespace Tower
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
